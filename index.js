4dac1a7e414a69641130ce8e54f70663eaffb349
codernoyan <coder.noyan@gmail.com>
2022-10-26 22:50:54 +0600

    added course details

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 // courses data
 const courses = require('./data/courses.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
   res.send('programming library server is running');
@@ -14,8 +15,14 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
   res.send(courses);
-})
+});
+
+app.get('/courses/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const singleCourseDetails = courseDetails.find(details => details.id === id);
+  res.send(singleCourseDetails);
+});
 
 app.listen(port, () => {
   console.log(`Programming Library Server Running On Port: ${port}`);
-})
+});
